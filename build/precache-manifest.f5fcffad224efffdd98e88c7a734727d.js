06f291e7cab40326042295ae7bc992d2f614acbc
Bob1883 <104830549+Bob1883@users.noreply.github.com>
2023-01-07 21:32:42 +0100

    Add files via upload

---
+++ build/precache-manifest.f5fcffad224efffdd98e88c7a734727d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e8c7663ccdce293aa2",
+    "url": "/static/css/main.1d521925.chunk.css"
+  },
+  {
+    "revision": "a3e8c7663ccdce293aa2",
+    "url": "/static/js/main.a3e8c766.chunk.js"
+  },
+  {
+    "revision": "11ddf35151b139977906",
+    "url": "/static/js/1.11ddf351.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42f3fa1570020e2810d281fcd69dc2cf",
+    "url": "/static/media/selection2.42f3fa15.jpg"
+  },
+  {
+    "revision": "73b3bf4e09903da10805a2e29cd3973b",
+    "url": "/static/media/selection1.73b3bf4e.jpg"
+  },
+  {
+    "revision": "1accb5fa95a8cc0b7bc1b8b43172f5f0",
+    "url": "/static/media/selection3.1accb5fa.jpg"
+  },
+  {
+    "revision": "ed5a83d71b8ab7f1368afb2487a67a4b",
+    "url": "/static/media/featuredHoodie1.ed5a83d7.jpg"
+  },
+  {
+    "revision": "7925a90dc19af3cbaefabd30aa4fb4b9",
+    "url": "/static/media/featuredHoodie2.7925a90d.jpg"
+  },
+  {
+    "revision": "f7abde79f63ab5c244e62afe0657e06c",
+    "url": "/static/media/featuredHoodie3.f7abde79.jpg"
+  },
+  {
+    "revision": "90d0abd4449a33f33a21c3fa079c664b",
+    "url": "/static/media/CottonIMG.90d0abd4.png"
+  },
+  {
+    "revision": "b3e2832c8732980055d0efc66dbc8365",
+    "url": "/static/media/cotton.b3e2832c.jpg"
+  },
+  {
+    "revision": "57efb1f0aaca73742584dd1f13d11fef",
+    "url": "/index.html"
+  }
+];
